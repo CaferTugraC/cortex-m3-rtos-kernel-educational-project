7765cc4b27ebae20cd804c3accddf100d8d26cbd
Cafer Tura Çetin <cafercetin.tr@gmail.com>
2026-06-13 20:12:54 +0300

    refactor: Expand system status enum

--- common/return_enum.h
+++ common/return_enum.h
@@ -3,12 +3,23 @@
 
 typedef enum
 {
-    OK = 0,
-    ERROR_INIT = 1,
-    WARNING = 2,
-    ERROR_STACK_OVERFLOW = 3,
-    INVALID_PARAM = 4,
-    REACHED_MAX_TASK = 5,
+    /* Success & Warnings*/
+    KERNEL_OK = 0,
+    KERNEL_WARNING = 1,
+
+    /* Task & Scheduling Errors*/
+    KERNEL_ERROR_INIT = 2,
+    KERNEL_ERROR_STACK_OVERFLOW = 3,
+    KERNEL_REACHED_MAX_TASK = 4,
+
+    /* Parameter & Memory Errors*/
+    KERNEL_ERROR_INVALID_PARAM = 5,
+    KERNEL_ERROR_NULL_PTR = 6,
+    KERNEL_ERROR_NO_MEMORY = 7,
+
+     /* Context & State Errors */
+    ERR_NOT_IN_ISR = 13,   /* Execution not allowed from ISR context */
+    ERR_IN_ISR = 14,       /* Execution only allowed from ISR context */
 
 } System_Status_t;
 
